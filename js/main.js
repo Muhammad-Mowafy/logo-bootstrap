e48f626d4ded375c4d2513d337ec0f8423324727
Muhammad-Mowafy <148198543+Muhammad-Mowafy@users.noreply.github.com>
2023-10-17 11:34:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+// function view() {
+
+//     let txt = document.getElementById("text").value;
+//  let pass = document.getElementById("pass").value;
+
+//     if (txt.length > 20 || pass.length > 10) {
+//         alert("Wrong Valid Just 20 Char");
+//     }
+//     else  {
+//         alert ("Ok Your Information Send To database");
+//     }
+
+// }
+
+
+// window.onload = function showing () {
+
+//     setInterval(function () {
+//         let sh = document.getElementById("show");
+//         sh.style.display = "block";
+//     },1000);
+
+// };
+
+$(document).ready(function () {
+    $("#contact").fadeIn(1000);
+    $("#blog").fadeIn(2000);
+    $("#owork").fadeIn(3000);
+    $("#abou").fadeIn(4000);
+    $("#home").fadeIn(5000);
+
+});
